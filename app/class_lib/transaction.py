903461b90a3d9393b49f3dd35c8a895e274065ae
Morey Wood <morey.wood@gmail.com>
2015-11-06 15:25:31 -0600

    Partially Working Trasaction logging

---
+++ app/class_lib/transaction.py
@@ -0,0 +1,38 @@
+'''
+Created Nov 5 2015
+
+Project:  
+
+@authors:  Paul Barrett, Morey Wood, Kristen Massey, Daniel Waddell
+
+################################################
+################################################
+'''
+
+import os
+import sys
+import sqlite3
+import warnings
+from peewee import *
+from db_config import *
+import account
+
+class Transaction(DatabaseModel):
+
+    ############################
+    ###  Initialize class object
+    ############################
+
+    time = DateField()
+    value = DoubleField()
+    to_account = ForeignKeyField(account.Account, related_name='to_transactions')
+    from_account = ForeignKeyField(account.Account, related_name='from_transactions')
+
+    #def __init__(self, log_content="", start=None, end=None):
+    #    return None
+
+    def __str__(self):
+        return "%s: %s transferred from %s to %s" % (time, value, from_account, to_account)
+
+
+   
