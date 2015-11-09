544ff05816d64c5b446556512bec8696bb675d09
Paul Barrett <pmbarrett314@gmail.com>
2015-11-09 13:59:26 -0600

    used absolute path in db_init

--- app/class_lib/db_config.py
+++ app/class_lib/db_config.py
@@ -1,7 +1,12 @@
 from peewee import *
+import os
 
-db = SqliteDatabase('bank.db')
+
+dbpath = os.path.join(os.path.dirname(os.path.realpath(__file__)),"..","bank.db")
+
+db = SqliteDatabase(dbpath)
+
 
 class DatabaseModel(Model):
-	class Meta:
-		database = db
+    class Meta:
+        database = db
