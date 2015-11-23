e202fb0831d95fe994babc48701cf5433fd434c5
krislmassey <kristenmassey@yahoo.com>
2015-11-23 10:24:13 -0600

    Stock.py works
    now grabbing live data successfully from tradier.

--- app/model/stock.py
+++ app/model/stock.py
@@ -5,12 +5,18 @@ Project:
 
 @authors:  Paul Barrett, Morey Wood, Kristen Massey, Daniel Waddell
 
+Sources:        https://developer.tradier.com/documentation/markets/get-quotes
+                https://developer.tradier.com/documentation/examples/python
+                https://developer.tradier.com/getting_started
+
 ################################################
 ################################################
 '''
 
 import urllib2
+import httplib
 import xml.etree.ElementTree as elementTree
+import json
 
 
 class Stock:
@@ -20,38 +26,67 @@ class Stock:
         symbol = ""
         current_price = 0.0
         description = ""
+        raw_data = ""
 
         def __init__(self, symbol):
                 self.symbol = symbol
-                print symbol + "\n"
-                tradier_xml = tradier_conn(symbol)
-                self.current_price = get_price(tradier_xml)
-                self.description = get_ticker_description(tradier_xml) #should be a description of the company name?
+
+                #get data from tradier
+                self.raw_data = tradier_conn(symbol)
+                tradier_dict = json.loads(self.raw_data)
+
+                #parse tradier data
+                self.current_price = get_price(tradier_dict)
+                self.description = get_ticker_description(tradier_dict) #should be a description of the company name?
                 return
 
 def tradier_conn(symbol):
-        conn = urllib2.Request('https://api.tradier.com/v1/markets/quotes?symbols=%s' % symbol)
-        conn.add_header('Authorization', 'Bearer AS3tVA7ZLKmG25NuY4aL7HhyRLME') #NOTE:  THE BEARER CODE MUST BE REACTIVATED EVERY 24 HOURS
-        result = urllib2.urlopen(conn)
-        return result
+
+        ###############################
+        ###  START OF TRADIER CODE  ###
+        ###############################
+        # Request: Market Quotes (https://sandbox.tradier.com/v1/markets/quotes?symbols=spy)
+
+        connection = httplib.HTTPSConnection('sandbox.tradier.com', 443, timeout = 30)
+
+        # Headers
+
+        headers = {"Accept":"application/json",
+                   "Authorization":"Bearer UAiiqlYjW80rzD0u5wC6FMAQMF4v"}
+
+        # Send synchronously
+
+        connection.request('GET', '/v1/markets/quotes?symbols=%s' % symbol, None, headers)
+        try:
+                response = connection.getresponse()
+                content = response.read()
+                # Success
+                #print('Response status ' + str(response.status))
+        except httplib.HTTPException, e:
+                # Exception
+                print('Exception during request')
+
+        #############################
+        ###  END OF TRADIER CODE  ###
+        #############################
+        return content
                 
 
-def get_price(xml_file):
-        root = elementTree.fromstring(xml_file)
-        element = root.findall('ask')
-        current_ask = element.attrib
-        print "Ask: " + current_ask + "\n"
+def get_price(tradier_dict):
+        current_ask = tradier_dict["quotes"]["quote"]["ask"]
         return current_ask
 
-def get_ticker_description(xml_file):
-        root = elementTree.fromstring(xml_file)
-        element = root.findall('description')
-        description = element.attrib
-        print "Description: " + description + "\n"
+def get_ticker_description(tradier_dict):
+        description = tradier_dict["quotes"]["quote"]["description"]
         return description
+
 
 def main():
         mystock = Stock("GOOG")
+        print "Stock is: " + str(mystock.description) + "\n"
+        print "Stock price: " + str(mystock.current_price) + "\n"
+        print "raw data: \n"
+        print mystock.raw_data
 
                                                                 
 if __name__ == '__main__':
