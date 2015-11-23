6c934396cc7581ff80ca5c0fce874c237ae627c9
krislmassey <kristenmassey@yahoo.com>
2015-11-22 18:18:35 -0600

    Tradier work - unfinished
    This is as far as I got after working all afternoon trying to figure out
    the ins and outs of the tradier API.  We have to get a connection to a
    web page which returns a string in xml format, and that string then has
    to be parsed to get the data we need.  I have the functions written out
    to get that data but right now when I run a test to get the info for
    google stocks, it crashes on line 35 with "HTTPError: HTTP ERROR 401:
    Unauthorized"
    
    I'm assuming that means it doesn't like the token I am passing it.  That
    probably has to do with how my tradier account is set up.  I am looking
    into it.  This model is close to being done as far as lines of code that
    have to be written but it's probably going to take a lot of little
    tweaks to get it to do the tradier connection and xml parsing.

--- app/model/stock.py
+++ app/model/stock.py
@@ -9,14 +9,60 @@ Project:
 ################################################
 '''
 
+import urllib2
+import xml.etree.ElementTree as elementTree
 
 
 class Stock:
-	############################
-    ###  Class Variables
-    ############################
-	symbol = ""
-	current_price = 0.0
+        ############################
+        ###  Class Variables
+        ############################
+        symbol = ""
+        current_price = 0.0
+        description = ""
 
-	def __init__(symbol):
-		self.symbol = symbol
+        def __init__(self, symbol):
+                self.symbol = symbol
+                print symbol + "\n"
+                tradier_xml = tradier_conn(symbol)
+                self.current_price = get_price(tradier_xml)
+                self.description = get_ticker_description(tradier_xml) #should be a description of the company name?
+                return
+
+def tradier_conn(symbol):
+        conn = urllib2.Request('https://api.tradier.com/v1/markets/quotes?symbols=%s' % symbol)
+        conn.add_header('Authorization', 'Bearer AS3tVA7ZLKmG25NuY4aL7HhyRLME') #NOTE:  THE BEARER CODE MUST BE REACTIVATED EVERY 24 HOURS
+        result = urllib2.urlopen(conn)
+        return result
+                
+
+def get_price(xml_file):
+        root = elementTree.fromstring(xml_file)
+        element = root.findall('ask')
+        current_ask = element.attrib
+        print "Ask: " + current_ask + "\n"
+        return current_ask
+
+def get_ticker_description(xml_file):
+        root = elementTree.fromstring(xml_file)
+        element = root.findall('description')
+        description = element.attrib
+        print "Description: " + description + "\n"
+        return description
+
+def main():
+        mystock = Stock("GOOG")
+
+                                                                
+if __name__ == '__main__':
+        main()
+
+
+        #Steps:
+        #       1) connect to tradier with access token
+        #       2) create query for market data on ticker symbol self.symbol
+        #       3) get xml file with data on self.symbol
+        #       4) parse xml for stock price
+        #       5) parse xml for company name
+                
+                
